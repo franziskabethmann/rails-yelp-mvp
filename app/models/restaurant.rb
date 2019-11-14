b604519707cec5a34ffb509ca27d71100c04983f
Franziska von Bethmann <franziska.bethmann@gmail.com>
2019-11-14 07:32:00 +0100

    created the yelp app

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  CATEGORIES = %w[chinese italian japanese french belgian]
 
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
